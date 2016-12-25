e81c6b5c2922b1f9ef9e5c83183da10b84ff4bdc
Yadab Raj Ojha <YadabRaj.Ojha@pointclickcare.com>
2016-12-25 14:55:57 +0545

    filter added

---
+++ src/javastud/servlet/filter/LogFilter.java
@@ -0,0 +1,56 @@
+package javastud.servlet.filter;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class LogFilter
+ */
+@WebFilter("/*")
+public class LogFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public LogFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// Get the IP address of client machine.
+		String ipAddress = request.getRemoteAddr();
+
+		// Log the IP address and current timestamp.
+		System.out.println("IP " + ipAddress + ", Time " + new Date().toString());
+
+		// Pass request back down the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
